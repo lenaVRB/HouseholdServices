13be810d7ebf6de3f0ceb7524f1c0125344ee5a5
Elena Vorobyova <wonderlenka@gmail.com>
2018-03-28 14:51:36 +0300

    Added infrastructure and UnitOfWork

---
+++ HouseholdServices.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseholdServices.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        HouseholdServiceModel dbContext;
+
+        public HouseholdServiceModel Init()
+        {
+            return dbContext ?? (dbContext = new HouseholdServiceModel());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ HouseholdServices.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseholdServices.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private HouseholdServiceModel dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public HouseholdServiceModel DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.Commit();
+        }
+    }
+}
