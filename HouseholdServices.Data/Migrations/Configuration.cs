665368b5e92a5ca63c848db9adeb84978b60aaf8
lenaVRB <wonderlenka@gmail.com>
2018-03-30 13:35:22 +0300

    config repository, start adding migrations

---
+++ HouseholdServices.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace HouseholdServices.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HouseholdServices.Data.HouseholdServiceModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HouseholdServices.Data.HouseholdServiceModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ HouseholdServices.Data/Repositories/EntityBaseRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HouseholdServices.Entities;
+using HouseholdServices.Data.Infrastructure;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace HouseholdServices.Data.Repositories
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T: class, IEntityBase, new()
+    {
+        private HouseholdServiceModel dbContext;
+
+        #region Properties
+        protected IDbFactory DbFactory
+        {
+            get;
+            private set;
+        }
+
+        protected HouseholdServiceModel DbContext
+        {
+            get { return dbContext ?? (dbContext = DbFactory.Init()); }
+        }
+
+        public EntityBaseRepository(IDbFactory dbFactory)
+        {
+            DbFactory = dbFactory;
+        }
+        #endregion
+
+        public virtual IQueryable<T> All
+        {
+            get
+            {
+                return GetAll();
+            }
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IQueryable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = DbContext.Set<T>();
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+            return query;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+            dbEntityEntry.State = EntityState.Deleted;
+        }
+
+        public virtual void Edit(T entity)
+        {
+            DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+            dbEntityEntry.State = EntityState.Modified;
+        }
+
+        public virtual IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            return DbContext.Set<T>().Where(predicate);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return DbContext.Set<T>();
+        }
+
+        public T GetSingle(int id)
+        {
+            return GetAll().FirstOrDefault(r => r.ID == id);
+        }
+    }
+}
