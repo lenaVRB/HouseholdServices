42561409ac94d8d2759abdd3ebe16ac89414052e
lenaVRB <wonderlenka@gmail.com>
2018-04-07 23:55:05 +0300

    AutofacWebapiConfig added

---
+++ HouseholdServices/App_Start/AutofacWebapiConfig.cs
@@ -0,0 +1,69 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Integration.WebApi;
+using HouseholdServices.Data;
+using HouseholdServices.Data.Infrastructure;
+using HouseholdServices.Data.Repositories;
+using HouseholdServices.Services.Abstract;
+using HouseholdServices.Entities;
+using HouseholdServices.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+
+namespace HouseholdServices.App_Start
+{
+    
+    public class AutofacWebapiConfig
+    {
+        public static IContainer Container;
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        public static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<HouseholdServiceModel>()
+                .As<DbContext>()
+                .InstancePerRequest();
+
+            builder.RegisterType<DbFactory>()
+                .As<IDbFactory>()
+                .InstancePerRequest();
+
+            builder.RegisterType<UnitOfWork>()
+                .As<IUnitOfWork>()
+                .InstancePerRequest();
+
+            builder.RegisterGeneric(typeof(EntityBaseRepository<>))
+                .As(typeof(IEntityBaseRepository<>))
+                .InstancePerRequest();
+
+            builder.RegisterType<EncryptionService>().As<IEncryptionService>().InstancePerRequest();
+
+            builder.RegisterType<MembershipService>()
+                .As<IMembershipService>()
+                .InstancePerRequest();
+
+            Container = builder.Build();
+
+            return Container;
+
+
+        }
+    }
+}
