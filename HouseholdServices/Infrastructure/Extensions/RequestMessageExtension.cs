dd04c264fab7a41b09f3279404405dc87b9db755
lenaVRB <wonderlenka@gmail.com>
2018-04-09 22:44:31 +0300

    front started

---
+++ HouseholdServices/Infrastructure/Extensions/RequestMessageExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using HouseholdServices.Services;
+using HouseholdServices.Services.Abstract;
+using HouseholdServices.Entities;
+using HouseholdServices.Data.Repositories;
+using System.Web.Http.Dependencies;
+
+namespace HouseholdServices.Infrastructure.Extensions
+{
+    public static class RequestMessageExtension
+    {
+        internal static IMembershipService GetMembershipService(this HttpRequestMessage request)
+        {
+            return request.GetService<IMembershipService>();
+        }
+
+        internal static IEntityBaseRepository<T> GetDataRepository<T>(this HttpRequestMessage request) where T: class,IEntityBase, new()
+        {
+            return request.GetService<IEntityBaseRepository<T>>();
+        } 
+
+        internal static TService GetService<TService> (this HttpRequestMessage request)
+        {
+            IDependencyScope dependencyScope = request.GetDependencyScope();
+            TService service = (TService)dependencyScope.GetService(typeof(TService));
+
+            return service;
+        }
+
+    }
+}
--- HouseholdServices/Infrastructure/MessageHandlers/HouseholdServicesAuthHandler.cs
+++ HouseholdServices/Infrastructure/MessageHandlers/HouseholdServicesAuthHandler.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using HouseholdServices.Infrastructure.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Security.Principal;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,9 +32,41 @@ namespace HouseholdServices.Infrastructure.MessageHandlers
                     string decodedString = Encoding.UTF8.GetString(data);
                     string[] tokenValues = decodedString.Split(':');
                     var membershipService = request.GetMembershipService();
-                }
-            }
 
+                    var membershipContext = membershipService.ValidateUser(tokenValues[0], tokenValues[1]);
+
+                    if(membershipContext.User != null)
+                    {
+                        IPrincipal principal = membershipContext.Principal;
+                        Thread.CurrentPrincipal = principal;
+                        HttpContext.Current.User = principal;
+                    }
+
+                    else // Unauthorized access - wrong credentials
+                    {
+                        var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                        var tsc = new TaskCompletionSource<HttpResponseMessage>();
+                        tsc.SetResult(response);
+                        return tsc.Task;
+                    }
+                }
+                else
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                    var taskComplSource = new TaskCompletionSource<HttpResponseMessage>();
+                    taskComplSource.SetResult(response);
+                    return taskComplSource.Task;
+                }
+                return base.SendAsync(request, cancellationToken);
+            }
+            catch
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                var taskComplSource = new TaskCompletionSource<HttpResponseMessage>();
+                taskComplSource.SetResult(response);
+                return taskComplSource.Task;
+
+            }
         }
 
     }
